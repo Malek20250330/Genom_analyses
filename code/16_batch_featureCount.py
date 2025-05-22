6d1026b512f8e0a4c3e2af26fe71254ce69e43c9
Mahtab Malek Zadeh <mahtam@rackham3.uppmax.uu.se>
2025-05-23 01:44:09 +0200

    v2

--- code/16_batch_featureCount.py
+++ code/16_batch_featureCount.py
@@ -6,7 +6,7 @@
 #SBATCH --mem=16G
 #SBATCH -t 20:00:00
 #SBATCH -J featureCounts
-#SBATCH --mail-user
+#SBATCH --mail-user=mah_75420@yahoo.com
 #SBATCH --output=%x.%j.out
 
 set -e  # Exit on error
@@ -14,20 +14,20 @@ set -x  # Print commands
 
 # Load modules
 module load bioinfo-tools
-module load subread  # contains featureCounts
+module load subread  
 #module load samtools  # for header inspection
 
 # Set paths
 export INPUT_HP_DIR=/proj/uppmax2025-3-3/GAP2_mahtab/BWA_HP126
-export INPUT_R_DIR=/proj/uppmax2025-3-3/GAP2_mahtab/BWA_R7
+#export INPUT_R_DIR=/proj/uppmax2025-3-3/GAP2_mahtab/BWA_R7
 export GFF_DIR=/home/mahtam/Genom_analyses/Annotation_data/Prokka_annotation_R7
-export OUTPUT_DIR=/proj/uppmax2025-3-3/GAP2_mahtab/FeatureCounts
+export OUTPUT_DIR=/proj/uppmax2025-3-3/GAP2_mahtab/FeatureCounts2
 #mkdir -p $OUTPUT_DIR
 
 
 # Copy data to temporary directory
-#cp $INPUT_HP_DIR/*.sorted.bam $SNIC_TMP/
-cp $INPUT_R_DIR/*.sorted.bam $SNIC_TMP/
+cp $INPUT_HP_DIR/*.sorted.bam $SNIC_TMP/
+#cp $INPUT_R_DIR/*.sorted.bam $SNIC_TMP/
 cp $GFF_DIR/R7_annotation.gff $SNIC_TMP/
 cd $SNIC_TMP
 
